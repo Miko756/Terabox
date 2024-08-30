e6ff6a70e0861e2f33f05e36fa9add9a04d4bae7
Sumitpatel08 <119158472+Sumitpatel08@users.noreply.github.com>
2024-08-30 08:35:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 API_ID =   "23400324" 
 API_HASH = "6dd43d5bba765cfe72191bf60e65926f" 
 
-BOT_TOKEN = ""
-MONGO_DB_URL=""
+BOT_TOKEN = "7194917305:AAH5kSBi8OLpbZ11e7rijCeFmu-RNFrcsIA"
+MONGO_DB_URL="mongodb+srv://sunitverma080:6ogQshfbgIMjVIFX@terabox.cym0k.mongodb.net/?retryWrites=true&w=majority&appName=Terabox"
 DB_NAME="Terabox"
 
 HOST = "redis://redis-15242.c13.us-east-1-3.ec2.redns.redis-cloud.com"  
