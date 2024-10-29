9954dc386de5bfa93e9047888f21425ff3b69aac
Yae Miko <164617880+Miko756@users.noreply.github.com>
2024-10-29 21:16:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
 API_ID =   "23400324" 
-API_HASH = "6dd43d5bba765cfe72191bf60e65926f" 
+API_HASH = "7716433955:AAFhH0_Y8ltddApzJGLSYbxvNqttwvz-6-8" 
 
-BOT_TOKEN = "7194917305:AAH5kSBi8OLpbZ11e7rijCeFmu-RNFrcsIA"
+BOT_TOKEN = "7716433955:AAFhH0_Y8ltddApzJGLSYbxvNqttwvz-6-8"
 MONGO_DB_URL="mongodb+srv://sunitverma080:6ogQshfbgIMjVIFX@terabox.cym0k.mongodb.net/?retryWrites=true&w=majority&appName=Terabox"
 DB_NAME="Terabot"
 
-HOST = "redis://redis-15242.c13.us-east-1-3.ec2.redns.redis-cloud.com"  
-PORT =   15242
-PASSWORD = "vSEEAvPXcz2k5abnKJ5Q7Y6c8rYSc69t"
+HOST = "redis-15799.c253.us-central1-1.gce.redns.redis-cloud.com"  
+PORT =   15799
+PASSWORD = "v54SGe7ABVtfYNwuSDTMnu9jXR5vWnqL"
 
-PRIVATE_CHAT_ID =   -1002172957745
+PRIVATE_CHAT_ID =   -1002423800055
 
-OWNER_ID = 7208538115
+OWNER_ID = 7092511418
 
-USER_CHANNEL = -1002222581548
+USER_CHANNEL = -1002325705306
+
